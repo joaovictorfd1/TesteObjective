f80fa0d030df6660ea305e2965a60e860502ad80
joaovictorfd1 <joaovictorffd1@gmail.com>
2022-04-25 12:15:37 -0300

    Teste de refatoração

---
+++ TesteObjective.js
@@ -0,0 +1,21 @@
+function Check (divisivel, nome) {
+    let diviseis = [3, 5]
+    diviseis.push(divisivel)
+    let saidas = ["fizz", "buzz"]
+    saidas.push(nome)
+    
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < diviseis.length; j++) {
+            if(i % diviseis[j] === 0 && diviseis[j] === 3) {
+                console.log("fizz");
+            } else if (i % diviseis[j] === 0 && diviseis[j] === 5) {
+                console.log("buzz");
+            } else if (i % diviseis[j] === 0 && diviseis[j] === divisivel) {
+                console.log(i);
+            }
+        }
+    }
+}
+
+Check(10, "João");
+
